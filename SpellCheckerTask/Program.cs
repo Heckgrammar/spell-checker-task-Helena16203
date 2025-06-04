c3604181529594df21edf207bef5319e724cc5a0
Helena16203 <144434544+Helena16203@users.noreply.github.com>
2025-06-04 17:51:32 +0100

    Update Program.cs

--- SpellCheckerTask/Program.cs
+++ SpellCheckerTask/Program.cs
@@ -10,39 +10,46 @@ namespace SpellCheckerTask
     {
         static void Main(string[] args)
         {
-            string[] words = createDictionary();
-            //1. Take a user input of a word an check if it has been spelled correctly
+                string[] words = createDictionary();
 
-            //2. Take a string of words as a user input and check they have all been spelled correctly
+                Console.WriteLine("Enter a sentence to check spelling:");
+                string userInput = Console.ReadLine();
+                string[] userWords = userInput.Split(' ', StringSplitOptions.RemoveEmptyEntries); //splits a sentence into individual words
 
-            //3.Create a spelling score based on the percentage of words spelled correctly
+                List<string> misspelledWords = new(); //creates a new list of strings
+                int correctCount = 0;
 
-            //4.Create a new list of words that have been spelled incorrectly and save it in a new file
+                foreach (string ActualWord in userWords)
+                {
+                    if (Array.Exists(words, w => w.Equals(ActualWord, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        correctCount++;
+                    }
+                    else
+                    {
+                        misspelledWords.Add(ActualWord);
+                    }
+                }
 
-            //Challenge - Hard task
+                double accuracy = (double)correctCount / userWords.Length * 100;
+                Console.WriteLine("Spelling Score: " + accuracy + ":F2}%");
 
-            //Try to work out which words the user is trying to spell by looking for similarities in
-            //the spelling and ask the user did they mean that.
-
-            //Add these suggested words to a spelling list that the user can save as a file to work on
-            //their own spelling
-
-
-
+                File.WriteAllLines("MisspelledWords.txt", misspelledWords);
         }
-        static string[] createDictionary()
-        {
-            using StreamReader words = new("WordsFile.txt");
-            int count = 0;
-            string[] dictionaryData = new string[178636];
-            while (!words.EndOfStream)
-            {
 
-                dictionaryData[count] = words.ReadLine();
-                count++;
-            }
-            words.Close();
-            return dictionaryData;
-        }
+                static string[] createDictionary()
+                {
+                    using StreamReader words = new("WordsFile.txt");
+                    int count = 0;
+                    string[] dictionaryData = new string[178636];
+                    while (!words.EndOfStream)
+                    {
+                        dictionaryData[count] = words.ReadLine();
+                        count++;
+                    }
+                    words.Close();
+                    return dictionaryData;
+                 }
     }
 }
+
